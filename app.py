9f6dc3d1b585d07397afc6e52fedc9c3e67896f0
Giuseppe D'Orazio <doraziog@hotmail.com>
2025-06-13 12:55:44 -0400

    added html layout

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.jinja_env.auto_reload = True
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!<p>"
+def landing():
+    return render_template("layout1.html")
