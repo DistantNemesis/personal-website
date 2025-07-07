de80dd4428b4495a0fe7285262a9ddd47684002d
Giuseppe D'Orazio <doraziog@hotmail.com>
2025-07-07 18:24:26 -0400

    Changed project structure, added "About me" section to homepage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,9 +2,9 @@ const catsBtn = document.getElementById("cats");
 const homeBtn = document.getElementById("home");
 
 catsBtn.addEventListener("click", function() {
-    window.location.href = "../templates/cats.html";
+    window.location.href = "/templates/cats.html";
 });
 
 homeBtn.addEventListener("click", function() {
-    window.location.href = "../templates/layout1.html";
+    window.location.href = "/index.html";
 });
