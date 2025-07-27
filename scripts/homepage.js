5308195bbef12756d130b7f86da80015e631d3d6
Giuseppe D'Orazio <doraziog@hotmail.com>
2025-07-27 13:03:14 -0400

    Added page for my interests, and a little div to show what I have added to the website each day

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,6 @@
 const catsBtn = document.getElementById("cats");
 const homeBtn = document.getElementById("home");
+const interestsBtn = document.getElementById("interests");
 
 catsBtn.addEventListener("click", function() {
     window.location.href = "/templates/cats.html";
@@ -7,5 +8,9 @@ catsBtn.addEventListener("click", function() {
 
 homeBtn.addEventListener("click", function() {
     window.location.href = "/index.html";
+});
+
+interestsBtn.addEventListener("click", function() {
+    window.location.href = "/templates/interests.html";
 });
 
